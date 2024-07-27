1f9b18f3a80e79cc2f86f24f7c3655b7d2c153f2
Isaquesantos7 <isaquesantos.1998@gmail.com>
2024-07-26 22:20:26 -0300

    feat: criado sidenav e estilizacao

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from './components/container/container.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    ContainerComponent
+    ContainerComponent,
+    SidenavComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,12 +1,35 @@
+import { NgClass, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faL, faHome, faClose } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    FontAwesomeModule,
+    NgIf,
+    NgClass,
+  ],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss'
 })
-export class SidenavComponent {
 
+export class SidenavComponent {
+  protected collapsed: boolean = false;
+
+  protected fal = faL;
+  protected faHome = faHome;
+  protected faClose = faClose;
+
+  public toggleCollapse(): void {
+    this.collapsed = !this.collapsed;
+  }
+
+  public closeSidenav(): void {
+    this.collapsed = false;
+  }
 }
