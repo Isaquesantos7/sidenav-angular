11f3d580f7fb060b9a4c85a401c497c87d410b59
Isaquesantos7 <isaquesantos.1998@gmail.com>
2024-07-26 21:20:21 -0300

    feat: add fontawesome

--- src/app/components/container/container.component.ts
+++ src/app/components/container/container.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-container',
   standalone: true,
-  imports: [],
+  imports: [ 
+    FontAwesomeModule
+  ],
   templateUrl: './container.component.html',
   styleUrl: './container.component.scss'
 })
 
 export class ContainerComponent {
-
+  faCoffee = faCoffee;
 }
